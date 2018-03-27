336d29c972aace92b89426ca143122422854be74
AesaKamar <aesa.kamar@gmail.com>
2018-03-27 10:03:17 -0400

    QuadTree initialization logic

--- src/something.elm
+++ src/something.elm
@@ -5,11 +5,11 @@
 module Main exposing (..)
 
 import Html exposing (Html)
-import Svg exposing (..)
-import Svg.Attributes exposing (..)
+import Svg exposing (rect, svg, g)
+import Svg.Attributes exposing (width, viewBox)
 import Window exposing (Size, resizes)
 import BoundingBox exposing (BoundingBox)
-import Math.Vector2 exposing (Vec2)
+import Math.Vector2 exposing (Vec2, vec2)
 
 
 main =
@@ -29,14 +29,10 @@ type alias NodeId =
     Int
 
 
-type alias Coord2d =
-    { x : Float, y : Float }
-
-
 type alias Node =
     { id : NodeId
     , mass : Mass
-    , coords : Coord2d
+    , coords : Vec2
     }
 
 
@@ -67,69 +63,6 @@ getQtBound q =
             x.bound
 
 
-
-{- | If node x does not contain a body, put the new body b here.
-   If node x is an internal node, update the center-of-mass and total mass of x. Recursively insert the body b in the appropriate quadrant.
-   If node x is an external node, say containing a body named c, then there are two bodies b and c in the same region. Subdivide the region further by creating four children. Then, recursively insert both b and c into the appropriate quadrant(s). Since b and c may still end up in the same quadrant, there may be several subdivisions during a single insertion. Finally, update the center-of-mass and total mass of x.
--}
-
-
-insertIntoBarnesHut : Node -> QuadTree -> QuadTree
-insertIntoBarnesHut n qt =
-    case qt of
-        QtEmptyLeaf x ->
-            QtOccupiedLeaf { bound = x.bound, occupant = n }
-
-        _ ->
-            QtEmptyLeaf { bound = (fromCorners ( 0, 0 ) ( 300, 300 )) }
-
-
-type alias Model =
-    { nodes : List NodeId
-    , edges : List ( NodeId, NodeId )
-    , quadTree : QuadTree
-    }
-
-
-fromCorners : ( Float, Float ) -> ( Float, Float ) -> BoundingBox
-fromCorners topLeft botRight =
-    BoundingBox.fromCorners (Math.Vector2.fromTuple topLeft) (Math.Vector2.fromTuple botRight)
-
-
-init : ( Model, Cmd Msg )
-init =
-    ( { nodes = []
-      , edges = []
-      , quadTree = QtEmptyLeaf { bound = (fromCorners ( 0, 0 ) ( 300, 300 )) }
-      }
-    , Cmd.none
-    )
-
-
-
--- UPDATE
-
-
-type Msg
-    = WindowResize Window.Size
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        WindowResize s ->
-            let
-                topLeft =
-                    ( 0, 0 )
-
-                botRight =
-                    ( toFloat s.width, toFloat s.height )
-            in
-                ( Model model.nodes model.edges (updateQtBound model.quadTree (fromCorners topLeft botRight))
-                , Cmd.none
-                )
-
-
 updateSubQtBounds : QtSubTrees -> BoundingBox -> QtSubTrees
 updateSubQtBounds originals newBound =
     let
@@ -154,6 +87,87 @@ updateQtBound originalQt newBound =
 
         QtEmptyLeaf x ->
             QtEmptyLeaf { bound = newBound }
+
+
+
+{- | If node x does not contain a body, put the new body b here.
+   If node x is an internal node, update the center-of-mass and total mass of x. Recursively insert the body b in the appropriate quadrant.
+   If node x is an external node, say containing a body named c, then there are two bodies b and c in the same region. Subdivide the region further by creating four children. Then, recursively insert both b and c into the appropriate quadrant(s). Since b and c may still end up in the same quadrant, there may be several subdivisions during a single insertion. Finally, update the center-of-mass and total mass of x.
+-}
+
+
+insertQt : Node -> QuadTree -> QuadTree
+insertQt n qt =
+    case qt of
+        QtEmptyLeaf x ->
+            QtOccupiedLeaf { bound = x.bound, occupant = n }
+
+        _ ->
+            QtEmptyLeaf { bound = (fromCorners ( 0, 0 ) ( 300, 300 )) }
+
+
+type alias Model =
+    { nodes : List NodeId
+    , edges : List ( NodeId, NodeId )
+    , quadTree : QuadTree
+    }
+
+
+fromCorners : ( Float, Float ) -> ( Float, Float ) -> BoundingBox
+fromCorners topLeft botRight =
+    BoundingBox.fromCorners (Math.Vector2.fromTuple topLeft) (Math.Vector2.fromTuple botRight)
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { nodes = initNodeIds
+      , edges = []
+      , quadTree = initialQuadTree
+      }
+    , Cmd.none
+    )
+
+
+initNodeIds : List NodeId
+initNodeIds =
+    [ 1, 2, 3, 4 ]
+
+
+initNodes : List Node
+initNodes =
+    (List.map (\i -> { id = i, mass = 1.0, coords = vec2 1.0 1.0 }) initNodeIds)
+
+
+initialQuadTree =
+    QtEmptyLeaf { bound = (fromCorners ( 0, 0 ) ( 1000, 1000 )) }
+
+
+insertedQuadTrees =
+    List.foldl (insertQt) initialQuadTree
+
+
+
+-- UPDATE
+
+
+type Msg
+    = WindowResize Window.Size
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        WindowResize s ->
+            let
+                topLeft =
+                    ( 0, 0 )
+
+                botRight =
+                    ( toFloat s.width, toFloat s.height )
+            in
+                ( Model model.nodes model.edges (updateQtBound model.quadTree (fromCorners topLeft botRight))
+                , Cmd.none
+                )
 
 
 
@@ -196,15 +210,43 @@ widthSvgAttr bb =
         width (toString wdth ++ "px")
 
 
+type alias ShouldDrawBoundingBox =
+    Bool
+
+
+viewQuadTree : QuadTree -> ShouldDrawBoundingBox -> Svg.Svg Msg
+viewQuadTree qt shouldDrawBoundingBox =
+    case qt of
+        QtEmptyLeaf params ->
+            Svg.g [] []
+
+        QtOccupiedLeaf params ->
+            Svg.g []
+                [ Svg.rect
+                    [ Svg.Attributes.width (toString (BoundingBox.width params.bound))
+                    , Svg.Attributes.height (toString (BoundingBox.height params.bound))
+                    ]
+                    []
+                ]
+
+        QtInternal params ->
+            Svg.g []
+                [ Svg.rect
+                    [ Svg.Attributes.width (toString (BoundingBox.width params.bound))
+                    , Svg.Attributes.height (toString (BoundingBox.height params.bound))
+                    ]
+                    []
+                , viewQuadTree params.subTrees.nw shouldDrawBoundingBox
+                , viewQuadTree params.subTrees.ne shouldDrawBoundingBox
+                , viewQuadTree params.subTrees.se shouldDrawBoundingBox
+                , viewQuadTree params.subTrees.sw shouldDrawBoundingBox
+                ]
+
+
 view : Model -> Html Msg
 view model =
     svg
         [ viewBoxSvgAttr (getQtBound model.quadTree)
         , widthSvgAttr (getQtBound model.quadTree)
         ]
-        [ text_
-            [ color "black"
-            , transform "translate(20,20)"
-            ]
-            [ text "Hello World" ]
-        ]
+        [ viewQuadTree model.quadTree True ]
